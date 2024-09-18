eeb5f89e1f7bae8fe9781a35003590c861e3042c
huyboon <huyht0704@gmail.com>
2024-09-18 22:25:18 +0700

    test send email with nestjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,14 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { Public } from 'src/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { MailerModule, MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService
+  ) { }
 
   @Post("login")
   @Public()
@@ -22,4 +26,17 @@ export class AuthController {
     return this.authService.handleRegister(registerDto)
   }
 
+
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService
+      .sendMail({
+        to: 'hbooncode74@gmail.com', // list of receivers
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>Hi, HuyBoonCode here</b>', // HTML body content
+      })
+    return "OK"
+  }
 }
