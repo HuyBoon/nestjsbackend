0a39a7a299e44f0e9a91c2b565ea416ac1e57bd4
huyboon <huyht0704@gmail.com>
2024-12-01 07:36:16 +0700

    a

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,34 +13,42 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
-    if (!user) return null
-    const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!isValidPassword) return null
-    return user;
+    if (!user) return null;
 
+    const isValidPassword = await comparePasswordHelper(pass, user.password);
+    if (!isValidPassword) return null;
+
+    return user;
   }
 
   async login(user: any) {
-    const payload = { username: user.email, sub: user._id };
+    // Include the role and other relevant fields in the payload
+    const payload = {
+      username: user.email,
+      sub: user._id,
+      role: user.role
+    };
+
     return {
       user: {
         email: user.email,
         _id: user._id,
-        name: user.name
+        name: user.name,
+        role: user.role, // Include role in the user object response
       },
       access_token: this.jwtService.sign(payload),
     };
   }
 
   handleRegister = async (registerDto: CreateAuthDto) => {
-    return await this.usersService.handleRegister(registerDto)
-  }
+    return await this.usersService.handleRegister(registerDto);
+  };
 
   checkCode = async (data: CodeAuthDto) => {
-    return await this.usersService.handleActive(data)
-  }
-  retryActive = async (data: string) => {
-    return await this.usersService.retryActive(data)
-  }
+    return await this.usersService.handleActive(data);
+  };
 
+  retryActive = async (data: string) => {
+    return await this.usersService.retryActive(data);
+  };
 }
